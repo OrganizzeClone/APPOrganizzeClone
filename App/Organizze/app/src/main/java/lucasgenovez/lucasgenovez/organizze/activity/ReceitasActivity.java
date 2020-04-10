6204f0f5ca310b890ff20a3b16ca858351c2ec44
LucasGenovez <projetoasfmu@gmail.com>
2020-04-10 18:15:53 -0300

    Update app  Registra o total da despesa do usuário e soma ao que ele já tinha naquele mês, trazendo a despesa total

--- App/Organizze/app/src/main/java/lucasgenovez/lucasgenovez/organizze/activity/ReceitasActivity.java
+++ App/Organizze/app/src/main/java/lucasgenovez/lucasgenovez/organizze/activity/ReceitasActivity.java
@@ -3,10 +3,24 @@ package lucasgenovez.lucasgenovez.organizze.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 import lucasgenovez.lucasgenovez.organizze.R;
+import lucasgenovez.lucasgenovez.organizze.config.ConfiguracaoFirebase;
+import lucasgenovez.lucasgenovez.organizze.model.Movimentacao;
 
 public class ReceitasActivity extends AppCompatActivity {
+
+    private TextInputEditText campoData, campoCategoria, campoDescricao;
+    private EditText campoValor;
+    private Movimentacao movimentacao;
+    private DatabaseReference firebaseRef = ConfiguracaoFirebase.getFirebaseDatabase();
+    private FirebaseAuth autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
+    private Double despesaTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
